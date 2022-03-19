5d993f4ae946efda5df4f2f9762d896c186c6652
surakat gideon <surakatg@gmail.com>
2022-03-19 22:47:38 +0100

    repository

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,48 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuggyController : BaseApiController
+    {
+        private readonly Datacontext _context;
+
+        public BuggyController(Datacontext context)
+        {
+            _context = context;
+        }
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+            if (thing == null) return NotFound();
+            return Ok(thing);
+          
+        }
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+
+            var thing = _context.Users.Find(-1);
+            var thingToReturn = thing.ToString();
+            return thingToReturn;
+        }
+        [HttpGet("Bad-Request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest();
+        }
+    }
+}
---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.Models;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser appuser);
+        Task<bool> SaveAllAsync ();
+        Task<IEnumerable<AppUser>> GetUsersAsync ();
+        Task<AppUser> GetIdAsync (int id);
+        Task<AppUser> GetUserByUserNameAsync(string username); 
+    }
+}
